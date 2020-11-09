74868ee86dbd1f3cca20da6a4297862f6142a785
Tessa Woodard <tessawoodard@outlook.com>
2020-11-09 11:37:43 -0500

    wip

---
+++ src/components/Globals/Navbar.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react'
+import { Link } from 'gatsby'
+import logo from '../../images/logo.svg'
+import { FaCartArrowDown } from 'react-icons/fa'
+
+export default class Navbar extends Component {
+    state = {
+        navbarOpen: false,
+        css: 'collapse navbar-collapse',
+        links: [
+            {
+                id: 1,
+                path: '/',
+                text: 'home'
+            }, {
+                id: 2,
+                path: '/about',
+                text: 'about'
+            }
+        ]
+    }
+
+    navbarHandler = () => {
+        this.state.navbarOpen
+            ? this.setState({
+                navbarOpen: false,
+                css: "collapse navbar-collapse"
+            })
+            : this.setState({
+                navbarOpen: true,
+                css: "collapse navbar-collapse show"
+            })
+    }
+
+    render() {
+        return (
+            <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
+                <Link to='/' className="navbar-brand">
+                    <img src={logo} alt="logo" />
+                </Link>
+                <button className="navbar-toggler"
+                    type="button"
+                    onClick={this.navbarHandler}
+                >
+                    <span className="navbar-toggler-icon" />
+                </button>
+                <div className={this.state.css}>
+                    <ul className="navbar-nav mx-auto">
+                        {this.state.links.map(link => {
+                            return (
+                                <li key={link.id} className="nav-item">
+                                    <Link to={link.path} className="nav-link text-capitalize ">
+                                        {link.text}
+                                    </Link>
+                                </li>
+                            )
+                        })}
+
+                        <li className="nav-item  ml-sm-5">
+                            <FaCartArrowDown className="cart-icon snipcart-checkout" />
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        )
+    }
+}
--- src/components/layout.js
+++ src/components/layout.js
@@ -4,8 +4,11 @@ import PropTypes from "prop-types"
 import "./bootstrap.min.css"
 import "./layout.css"
 
+import Navbar from './Globals/Navbar'
+
 const Layout = ({ children }) => (
   <>
+    <Navbar />
     {children}
   </>
 )
