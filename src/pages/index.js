1215b67a866ccfbc3cef51a74bb77fd72a237e1c
Tessa Woodard <tessawoodard@outlook.com>
2020-11-09 12:29:12 -0500

    wip

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,35 @@
 import React from "react"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { FaGulp } from "react-icons/fa"
+import BackgroundSection from '../components/Globals/BackgroundSection'
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <FaGulp />
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-    </div>
+    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <BackgroundSection
+      img={data.img.childImageSharp.fluid}
+      title="Impresso Espresso"
+      styleClass="default-background"
+    />
+    {/* <Info />
+    <Menu items={data.menu} />
+    <Products />
+    <Contact /> */}
   </Layout>
 )
+
+export const query = graphql`
+{
+  img: file(relativePath:{eq: "default-background.jpeg" }){
+    childImageSharp {
+      fluid {
+        ...GatsbyImageSharpFluid_tracedSVG
+      }
+    }
+  }
+}
+`
 
 export default IndexPage
