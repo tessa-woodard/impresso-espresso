62138f7140c74fa59c7d2b88134bb7caa53afa1b
Tessa Woodard <tessawoodard@outlook.com>
2020-11-13 01:28:01 -0500

    wip

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,12 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 import BackgroundSection from '../components/Globals/BackgroundSection'
-// import Hero from '../components/Globals/Hero'
+import Hero from '../components/Globals/Hero'
 
 const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    {/* <Hero /> */}
+    <Hero />
     <BackgroundSection
       img={data.img.childImageSharp.fluid}
       title="Impresso Espresso"
