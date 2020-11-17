7f8286912e377ed2bb9e5aa9c488a6db299e375b
Tessa Woodard <tessawoodard@outlook.com>
2020-11-16 20:36:39 -0500

    wip

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,11 +3,12 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import Error from "../components/Globals/404/Error"
+
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Error />
   </Layout>
 )
 
