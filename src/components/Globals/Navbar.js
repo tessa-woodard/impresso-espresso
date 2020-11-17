1ea26f69ed9c3784e9a8ed2310b289ae8f5f53c6
Tessa Woodard <tessawoodard@outlook.com>
2020-11-17 12:40:08 -0500

    wip

--- src/components/Globals/Navbar.js
+++ src/components/Globals/Navbar.js
@@ -1,79 +1,84 @@
-import React, { Component } from 'react'
-import { Link } from 'gatsby'
-import logo from '../../images/logo.svg'
-import { FaCartArrowDown } from 'react-icons/fa'
+import React, { Component } from "react"
+import { Link } from "gatsby"
+import logo from "../../images/logo.svg"
+import { FaCartArrowDown } from "react-icons/fa"
 
 export default class Navbar extends Component {
-    state = {
-        navbarOpen: false,
-        css: 'collapse navbar-collapse',
-        links: [
-            {
-                id: 1,
-                path: '/',
-                text: 'home'
-            }, {
-                id: 2,
-                path: '/menu',
-                text: 'our menu'
-            }, {
-                id: 3,
-                path: '/products',
-                text: 'products'
-            }, {
-                id: 4,
-                path: '/about',
-                text: 'about'
-            }, {
-                id: 5,
-                path: '/contact',
-                text: 'contact us'
-            }
-        ]
-    }
+  state = {
+    navbarOpen: false,
+    css: "collapse navbar-collapse",
+    links: [
+      {
+        id: 1,
+        path: "/",
+        text: "home",
+      },
+      {
+        id: 2,
+        path: "/menu",
+        text: "our menu",
+      },
+      {
+        id: 3,
+        path: "/products",
+        text: "products",
+      },
+      {
+        id: 4,
+        path: "/about",
+        text: "about",
+      },
+      {
+        id: 5,
+        path: "/contact",
+        text: "contact us",
+      },
+    ],
+  }
 
-    navbarHandler = () => {
-        this.state.navbarOpen
-            ? this.setState({
-                navbarOpen: false,
-                css: "collapse navbar-collapse"
-            })
-            : this.setState({
-                navbarOpen: true,
-                css: "collapse navbar-collapse show"
-            })
-    }
+  navbarHandler = () => {
+    this.state.navbarOpen
+      ? this.setState({
+          navbarOpen: false,
+          css: "collapse navbar-collapse",
+        })
+      : this.setState({
+          navbarOpen: true,
+          css: "collapse navbar-collapse show",
+        })
+  }
 
-    render() {
-        return (
-            <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
-                <Link to='/' className="navbar-brand">
-                    <img src={logo} alt="logo" />
-                </Link>
-                <button className="navbar-toggler"
-                    type="button"
-                    onClick={this.navbarHandler}
-                >
-                    <span className="navbar-toggler-icon" />
-                </button>
-                <div className={this.state.css}>
-                    <ul className="navbar-nav mx-auto">
-                        {this.state.links.map(link => {
-                            return (
-                                <li key={link.id} className="nav-item">
-                                    <Link to={link.path} className="nav-link text-capitalize">
-                                        {link.text}
-                                    </Link>
-                                </li>
-                            )
-                        })}
+  render() {
+    return (
+      <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
+        <Link to="/" className="navbar-brand">
+          <img src={logo} alt="logo" />
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          onClick={this.navbarHandler}
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className={this.state.css}>
+          <ul className="navbar-nav mx-auto">
+            {this.state.links.map(link => {
+              return (
+                <li key={link.id} className="nav-item">
+                  <Link to={link.path} className="nav-link text-capitalize">
+                    {link.text}
+                  </Link>
+                </li>
+              )
+            })}
 
-                        <li className="nav-item ml-sm-5">
-                            <FaCartArrowDown className="cart-icon snipcart-checkout" />
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        )
-    }
+            <li className="nav-item ml-sm-5">
+              <FaCartArrowDown className="cart-icon snipcart-checkout" />
+            </li>
+          </ul>
+        </div>
+      </nav>
+    )
+  }
 }
